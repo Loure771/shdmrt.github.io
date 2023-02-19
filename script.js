2fe0195012a5f4a727a8375f26d604579a374aad
Loure771 <93453370+Loure771@users.noreply.github.com>
2023-02-20 00:15:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".multiple-text", {
     strings:["Fronted Geliştiricisiyim","YouTuberim","E-sporcuyum"],
-    typeSpeed:20,
-    backSpeed:20,
+    typeSpeed:60,
+    backSpeed:50,
     backDelay:1000,
     loop:true
 })
